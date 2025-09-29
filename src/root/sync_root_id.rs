0572b14ac61a3aecc579a1fd8c76581bf98d0918
Kingsword <kingsword09@gmail.com>
2025-09-29 12:58:06 +0800

    fix: resolve Rust 1.90.0 compatibility issue with TOKEN_USER handling

--- src/root/sync_root_id.rs
+++ src/root/sync_root_id.rs
@@ -256,31 +256,36 @@ impl SecurityId {
     pub fn current_user() -> core::Result<Self> {
         unsafe {
             let mut token_size = 0;
-            let mut token = MaybeUninit::<TOKEN_USER>::uninit();
 
             // get the token size
-            if let Err(e) = GetTokenInformation(
+            let info = GetTokenInformation(
                 Self::CURRENT_THREAD_EFFECTIVE_TOKEN,
                 Security::TokenUser,
                 None,
                 0,
                 &mut token_size,
-            ) {
+            );
+
+            if let Err(e) = info {
                 if e.code() != ERROR_INSUFFICIENT_BUFFER.to_hresult() {
                     Err(e)?;
                 }
-                GetTokenInformation(
-                    Self::CURRENT_THREAD_EFFECTIVE_TOKEN,
-                    Security::TokenUser,
-                    Some(&mut token as *mut _ as *mut _),
-                    token_size,
-                    &mut token_size,
-                )?;
             }
 
-            let token = token.assume_init();
+            let mut buffer = Vec::<MaybeUninit<u8>>::with_capacity(token_size as usize);
+            buffer.set_len(token_size as usize);
+
+            GetTokenInformation(
+                Self::CURRENT_THREAD_EFFECTIVE_TOKEN,
+                Security::TokenUser,
+                Some(buffer.as_mut_ptr() as *mut _),
+                token_size,
+                &mut token_size,
+            )?;
+
+            let token_user = &*(buffer.as_ptr() as *const TOKEN_USER);
             let mut sid = PWSTR(ptr::null_mut());
-            ConvertSidToStringSidW(token.User.Sid, &mut sid as *mut _)?;
+            ConvertSidToStringSidW(token_user.User.Sid, &mut sid as *mut _)?;
 
             let string_sid = U16CStr::from_ptr_str(sid.0).to_os_string();
             LocalFree(HLOCAL(sid.0 as *mut _));
